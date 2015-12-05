465a4e0313022f456bde8a74a7d0105c8071bad6
edindlado <edinaldofox@gmail.com>
2015-12-05 14:16:23 -0300

    adicionado arquivos para o login

---
+++ src/controller/Main.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import static javafx.application.Application.launch;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author edinaldo
+ */
+public class Main extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+        try {
+
+            Parent root = FXMLLoader.load(Main.class.getResource("/view/Login.fxml"));
+           
+            Scene scene = new Scene(root);
+            
+            stage.getIcons().add(new Image("/imagens/icon.png"));
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.setTitle("Login / Cinema e Ingressos");
+            stage.show();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
