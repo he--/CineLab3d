d485d58c353f35530fac86bdf69f4995a6b19052
he-- <heliosouza@gmail.com>
2015-12-09 19:55:21 -0300

    Metodo para alocar um cliente a uma cadeira

--- src/cine/Sala.java
+++ src/cine/Sala.java
@@ -39,5 +39,15 @@ public class Sala {
 	{
 		return this.cadeiras;
 	}
+	
+	public boolean setCliente(Legenda legenda, Cliente cliente)
+	{
+		Cadeira cadeira = this.getCadeira(legenda);
+		if (cadeira.isOcupada()) {
+			return false;
+		}
+		cadeira.setCliente(cliente);
+		return true;
+	}
 
 }
